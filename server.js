097570ec615991862f385cdf7bb1b1bccb5ecebd
slamDuncan52 <dunc0474@kettering.edu>
2015-06-29 00:57:52 -0400

    Able to successfully send commands and receive standard output. Need to get it to maintain an environment and cwd, as well as start thinking security

--- server.js
+++ server.js
@@ -1,41 +1,49 @@
 /*----Twilio Account Info----*/
 var accountSID = "AC1662bc8ab0871857df0b5e53021d9c4b";
 var authToken = "c1fea3932ed9c1ccae6fc3f34a3356da";
-
 /*----Requires----*/
-var twilio = require('twilio')
-var twilioClient = twilio(accountSID, authToken);
+var twilio = require('twilio');
+var fs = require('fs');
 var exec = require('child_process').exec,child;
-var http = require('http');
+var express = require('express');
+var parser = require('body-parser');
 
+/*----Global Vars----*/
+var twilioClient = twilio(accountSID, authToken);
 var twiRes = twilio.TwimlResponse();
-http.createServer(function(req,res){
-        twiRes.say({voice:'woman'}, 'Welcome to Acme Corporation!');
-        res.writeHead(200,{
-                'Content-Type':'text/xml'
-        });
-        res.end(twiRes.toString());
-}).listen(1337);
+var twiNumber = "+18102750107";
+var http = express();
+var curDir = "/home";
+var environ = process.env;
 
-function textCommand(command,cb){
-        exec(command, function(err,stdout,stderr){
-                if(!stdout){
-                        stdout = command+" finished\n";
-                }
-                twilioClient.messages.create({
-                        body: stdout,
-                        to: "+18108755224",
-                        from: "+18102750107",
-                },function(err,msg){
-                        if(err){
-                                console.log(err);
-                        } else {
-                                console.log(msg.body);
-                                if(cb){
-                                        cb();
-                                }
-                        }
-                });
-        });
+http.use(parser.json());
+http.use(parser.urlencoded({extended: true}));
+
+http.post('/',function(req,res){
+	textCommand(req.body.Body,req.body.From);
+});
+
+var server = http.listen(3000, function () {
+	var host = server.address().address;
+	var port = server.address().port;
+});
+
+function textCommand(command,phoneNumber,cb){
+	exec(command,{cwd: curDir},
+	     function(err,stdout,stderr){
+		     if(stdout === ""){
+			     stdout = command+" finished\n";
+		     }
+		     twilioClient.messages.create({
+			     body: stdout,
+			     to: phoneNumber,
+			     from: twiNumber,
+		     },function(err,msg){
+			     if(err){
+				     console.log(err);
+			     } else if(cb){
+				     cb();
+			     }
+		     });
+	     });
 }
-textCommand("cd ~/.vim/bundle",function(){textCommand("ls")});
